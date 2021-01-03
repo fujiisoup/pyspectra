4b60fac9f94de85020e3f18a6d18182d4c3c03c5
Keisuke Fujii <fujiisoup@gmail.com>
2021-01-03 15:25:34 +0900

    added deconvolve into __init__

--- pyspectra/__init__.py
+++ pyspectra/__init__.py
@@ -1,2 +1,2 @@
-from . import atoms, molecules, profiles, refractive_index, units
+from . import atoms, molecules, profiles, refractive_index, units, deconvolve
 from .fit import singlepeak_fit, multipeak_nnls
