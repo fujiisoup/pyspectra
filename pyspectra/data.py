64652df19c3abb254137e24f12878e41e1529c27
Keisuke Fujii <fujiisoup@gmail.com>
2022-12-27 14:27:46 -0500

    Deal with the unicode index support in Pandas

--- pyspectra/data.py
+++ pyspectra/data.py
@@ -169,20 +169,39 @@ def download_diatomic_molecule_nist(molecule):
     def to_xarray(table):
         n = len(table.index)
         data = []
-        float_columns = {
-            "Te": "Te",
-            "e": "we",
-            "exe": "wexe",
-            "eye": "weye",
-            "Be": "Be",
-            "e.1": "alpha_e",
-            "e.2": "gamma_e",
-            "De": "De",
-            "e.3": "beta_e",
-            "re": "re",
-            "Trans.": "trans.",
-            "00": "nu00",
-        }
+        
+        pandas_version = [int(v) for v in pd._version.get_versions()['version'].split('.')]
+        if pandas_version[0] > 1 or (pandas_version[0] == 1 and pandas_version[1] > 3):
+            # 'State', 'Te', 'ωe', 'ωexe', 'ωeye', 'Be', 'αe', 'γe', 'De', 'βe', 're', 'Trans.', 'ν00'
+            float_columns = {
+                "Te": "Te",
+                "ωe": "we",
+                "ωexe": "wexe",
+                "ωeye": "weye",
+                "Be": "Be",
+                "αe": "alpha_e",
+                "γe": "gamma_e",
+                "De": "De",
+                "βe": "beta_e",
+                "re": "re",
+                "Trans.": "trans.",
+                "ν00": "nu00",
+            }
+        else:
+            float_columns = {
+                "Te": "Te",
+                "e": "we",
+                "exe": "wexe",
+                "eye": "weye",
+                "Be": "Be",
+                "e.1": "alpha_e",
+                "e.2": "gamma_e",
+                "De": "De",
+                "e.3": "beta_e",
+                "re": "re",
+                "Trans.": "trans.",
+                "00": "nu00",
+            }
         string_columns = {
             "State": "state",
         }
